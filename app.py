60291ea7629eaee3bcee75293ab13773367f6bbd
forloop.c <v.cycrun@gmail.com>
2024-08-22 18:34:59 +0530

    model learning7

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st # type: ignore
 from Prediction import show_predict_page
 from explore_page import show_explore_page
 
+
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
-if page == 'Predict':
+
+if page == "Predict":
     show_predict_page()
-else :
+else:
     show_explore_page()
--- explore_page.py
+++ explore_page.py
@@ -1,6 +1,6 @@
-import streamlit as st # type: ignore
-import pandas as pd # type: ignore
-import matplotlib.pyplot as plt # type: ignore
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def shorten_categories(categories, cutoff):
     categorical_map = {}
@@ -11,12 +11,14 @@ def shorten_categories(categories, cutoff):
             categorical_map[categories.index[i]] = 'Other'
     return categorical_map
 
+
 def clean_experience(x):
     if x ==  'More than 50 years':
         return 50
     if x == 'Less than 1 year':
         return 0.5
     return float(x)
+
 
 def clean_education(x):
     if 'Bachelor’s degree' in x:
@@ -27,58 +29,62 @@ def clean_education(x):
         return 'Post grad'
     return 'Less than a Bachelors'
 
-@st.cache_data
+
+@st.cache
 def load_data():
-    df = pd.read_csv('survey_results_public.csv')
-    df = df[['Country', 'EdLevel', 'YearsCodePro', 'Employment', 'ConvertedComp']]
-    df = df[df['ConvertedComp'].notnull()]
+    df = pd.read_csv("survey_results_public.csv")
+    df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedComp"]]
+    df = df[df["ConvertedComp"].notnull()]
     df = df.dropna()
     df = df[df["Employment"] == "Employed full-time"]
     df = df.drop("Employment", axis=1)
 
-
     country_map = shorten_categories(df.Country.value_counts(), 400)
-    df['Country'] = df['Country'].map(country_map) 
+    df["Country"] = df["Country"].map(country_map)
     df = df[df["ConvertedComp"] <= 250000]
-    df = df[df["ConvertedComp"] >=10000]
-    df = df[df['Country'] != "Other"]
+    df = df[df["ConvertedComp"] >= 10000]
+    df = df[df["Country"] != "Other"]
 
-    df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
-    df['EdLevel'] = df['EdLevel'].apply(clean_education)
+    df["YearsCodePro"] = df["YearsCodePro"].apply(clean_experience)
+    df["EdLevel"] = df["EdLevel"].apply(clean_education)
     df = df.rename({"ConvertedComp": "Salary"}, axis=1)
     return df
+
 df = load_data()
 
 def show_explore_page():
     st.title("Explore Software Engineer Salaries")
 
-    st.write("""
-             ###Stack Overflow Developer Survey 2020
-             """)
+    st.write(
+        """
+    ### Stack Overflow Developer Survey 2020
+    """
+    )
+
     data = df["Country"].value_counts()
 
     fig1, ax1 = plt.subplots()
-    # fig1.patch.set_facecolor('none')
-    # ax1.set_facecolor('none') 
-    colors = ['#cfe2f3', '#f4cccc', '#d9ead3', '#fff2cc', '#d0e0e3', '#cfe2f3', '#f9cb9c']
-    ax1.pie(data, labels=data.index, autopct='%1.1f%%', shadow=True, startangle=90, labeldistance=1.1, pctdistance=0.85)
-    # centre_circle = plt.Circle((0,0),0.70,fc='none')
-    # fig1.gca().add_artist(centre_circle)
-    ax1.axis('equal')
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-    st.write(''' Number of Data from different Countries''')
+    st.write("""#### Number of Data from different countries""")
 
     st.pyplot(fig1)
+    
+    st.write(
+        """
+    #### Mean Salary Based On Country
+    """
+    )
 
-    st.write('''
-             Mean Salary Based On Country
-             ''')
-    data = df.groupby(['Country'])['Salary'].mean().sort_values(ascending=True)
+    data = df.groupby(["Country"])["Salary"].mean().sort_values(ascending=True)
     st.bar_chart(data)
 
-    st.write('''
-             Mean Salary Based On Experience
-             ''')
-    
-    data = df.groupby(['YearsCodePro'])['Salary'].mean().sort_values(ascending=True)
+    st.write(
+        """
+    #### Mean Salary Based On Experience
+    """
+    )
+
+    data = df.groupby(["YearsCodePro"])["Salary"].mean().sort_values(ascending=True)
     st.line_chart(data)
